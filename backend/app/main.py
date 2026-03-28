d2767a8036b31f34df78674604df460284278b06
Miguel Layos.JS <miguelpogilayos@gmail.com>
2026-03-29 03:39:14 +0800

    test 1

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,37 +1,7 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import chat, file_upload, login
-from app.services.loaders import warmup
+app = FastAPI()
 
-from dotenv import load_dotenv
-load_dotenv() 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Runs once per worker when the process starts
-   # warmup()
-    yield
-
-app = FastAPI(lifespan=lifespan)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Include the routers
-app.include_router(chat.router)
-app.include_router(file_upload.router)
-app.include_router(login.router)
-
-# Dev
-# uvicorn app.main:app --reload
-
-#prod
-#uvicorn app.main:app --host 0.0.0.0 --port $PORT
+@app.get("/")
+def read_root():
+    return {"hello": "world"}
